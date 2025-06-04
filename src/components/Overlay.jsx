12050f0e01a5a5bdb07e2f82c51be8decbbc37e3
Varun24-s <varunsharma2990@gmail.com>
2025-06-04 13:47:24 +0530

    overlayZ

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -25,7 +25,7 @@ function Overlay() {
 
     return (
         <motion.div
-            className="fixed top-0 left-0 w-full h-screen pointer-events-none z-[100]"
+            className="fixed top-0 left-0 w-full h-screen pointer-events-none z-[140]"
             style={{ opacity }}
         >
             <svg
